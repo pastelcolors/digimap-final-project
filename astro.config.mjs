6b839011b38e7085cfa7830fbe12323056d4a529
Adrian Dayrit <itsadriandayrit@gmail.com>
2023-04-10 17:03:59 +0800

    fix: remove trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
   integrations: [tailwind(), solidJs()],
   vite: {
     plugins: [thalerPlugin.vite({
-      origin: 'https://digimap-final-project.vercel.app/',
+      origin: 'https://digimap-final-project.vercel.app',
       mode: 'server'
     })]
   }
